08bc86c18d0a77bb93b2d13b9324246634e5580a
Chaz Kondo <Chaz@Macbook-Pro.local>
2018-09-13 16:26:01 -1000

    Create database owner to has_man_user

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -2,3 +2,9 @@
 
 CREATE USER has_many_user;
 
+--Create a new database named has_many_blogs owned by has_many_user
+--DROP DATABASE has_many_blogs;
+
+CREATE DATABASE has_many_blogs;
+ALTER DATABASE has_many_blogs 
+OWNER TO has_many_user;
